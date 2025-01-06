8445183d2e7185b0f29c47ae500b650de1c8731c
Marcin Izdebski <mizdebski123@gmail.com>
2025-01-06 21:44:26 +0100

    Background changed

--- src/core/StyledComponents/globalStyles.ts
+++ src/core/StyledComponents/globalStyles.ts
@@ -13,7 +13,7 @@ html {
   }
   
 body {
-    background: #fff;
+    background: radial-gradient(circle, rgba(251,63,180,0.16290266106442575) 0%, rgba(70,196,252,1) 100%);
     font-family: 'Playfair Display', serif;
     margin: 0;
 }`;
