d7dd8dc20f0903e440af8a3f338f2e1fc68d6411
Marcin Izdebski <mizdebski123@gmail.com>
2025-01-06 15:39:57 +0100

    Theme provider & global styles added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import App from './core/App.tsx'
+import App from "./core/App.tsx";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./core/StyledComponents/theme.ts";
+import { GlobalStyles } from "./core/StyledComponents/globalStyles.ts";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <App />
+        </ThemeProvider>
+    </StrictMode>
+);
